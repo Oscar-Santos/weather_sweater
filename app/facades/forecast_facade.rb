f0f35e09972a2b66c09780fe9bc0e3355970f746
Oscar Santos <oscar303data@gmail.com>
2022-08-06 08:20:39 -0600

    create get_daily_weather method to retrieve daily weather

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -3,6 +3,13 @@ class ForecastFacade
     all_weather = ForecastService.get_city_weather(location)
 
     current = CurrentForecast.new(all_weather[:current])
+    daily = get_daily_weather(all_weather[:daily])
   end
 
+  def self.get_daily_weather(weather)
+    weather[0..4].map do |daily_weather|
+      DailyForecast.new(daily_weather)
+    end
+  end
+  
 end

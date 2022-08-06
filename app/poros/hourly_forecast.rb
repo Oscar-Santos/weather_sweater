21169ed449c7f5d7c592d6090de667b8d6a9e737
Oscar Santos <oscar303data@gmail.com>
2022-08-06 08:57:35 -0600

    create poros hourly forecast

---
+++ app/poros/hourly_forecast.rb
@@ -0,0 +1,13 @@
+class HourlyForecast
+  attr_reader :time,
+              :temperature,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @time = data[:dt]
+    @temperature = data[:temp]
+    @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:description]
+  end
+end

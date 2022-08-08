b654cd8c2dba09f91a4656d14a6fb431edbd6bcd
Oscar Santos <oscar303data@gmail.com>
2022-08-08 10:31:51 -0600

    munchies serializer returns information

--- app/serializers/api/v1/munchies_serializer.rb
+++ app/serializers/api/v1/munchies_serializer.rb
@@ -1,5 +1,22 @@
 class Api::V1::MunchiesSerializer
   def self.information_index(chinese_details)
+    {
+  data: {
+    id: nil,
+    type: "munchie",
+    attributes: {
+      "destination_city": "Pueblo, CO",
+      "forecast": {
+        "summary": "Cloudy with a chance of meatballs",
+        "temperature": "83"
+      },
+      restaurant: {
+        name: chinese_details.name,
+        address: chinese_details.address
+      }
+    }
+  }
+}
 
   end
 end

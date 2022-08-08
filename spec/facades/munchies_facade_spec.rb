59bc8888d86ac253cef24c564cd9f644c74afb1d
Oscar Santos <oscar303data@gmail.com>
2022-08-08 11:25:20 -0600

    create test for munchies service

--- spec/facades/munchies_facade_spec.rb
+++ spec/facades/munchies_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MunchiesFacade do
-  it 'forecast of Pueblo,Co' do
+  it 'forecast of Pueblo,Co', :vcr do
     forecast = MunchiesFacade.get_details('denver,co', 'chinese', 'Pueblo,co')
 
     expect(forecast.summary).to be_a(String)

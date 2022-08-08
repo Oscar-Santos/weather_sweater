fff98214085bd1424af38cbdb641544819f21811
Oscar Santos <oscar303data@gmail.com>
2022-08-08 10:29:02 -0600

    create munchies serializer

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,5 +2,10 @@ class Api::V1::MunchiesController < ApplicationController
   def index
     destination = "pueblo,co"
     location = MunchiesFacade.get_details(params[:location], params[:food], destination)
+
+    json_1 = MunchiesService.get_destination_details(params[:location], params[:food], destination)
+    chinese_details = Restaurant.new(json_1[:businesses][1])
+
+    render json: Api::V1::MunchiesSerializer.information_index(chinese_details)
   end
 end
---
+++ app/serializers/api/v1/munchies_serializer.rb
@@ -0,0 +1,5 @@
+class Api::V1::MunchiesSerializer
+  def self.information_index(chinese_details)
+
+  end
+end
